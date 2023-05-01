27da2c9b82ddfe722dffd9aaaee0f8d607da0754
nicon26 <116830194+nicon26@users.noreply.github.com>
2023-05-01 11:16:34 +0300

    implemented the Matrix class

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,43 +9,49 @@ public class App
         int choice;
 
         boolean ok = true;
-        while(ok) { ///// get input from the user
+        while(ok) {
+
             System.out.println("Select your action: ");
             Scanner in = new Scanner(System.in);
             choice = in.nextInt();
 
             switch (choice) {
+
                 case 1: // simple matrix
                     System.out.println("You chose a simple matrix.");
                     int rows1, columns1;
+                    System.out.println("Enter the number of rows and columns: ");
                     rows1 = in.nextInt();
                     columns1 = in.nextInt();
                     Matrix matrix = new Matrix(rows1, columns1);
                     matrix.calculateMatrix();
                     matrix.print();
                     break;
+
                 case 2: // square matrix
                     System.out.println("You chose a square matrix.");
                     int rows2;
+                    System.out.println("Enter the number of rows and columns: ");
                     rows2 = in.nextInt();
                     SquareMatrix squareMatrix = new SquareMatrix(rows2);
                     squareMatrix.calculateMatrix();
                     squareMatrix.print();
                     break;
+
                 case 3: // letter matrix
                     System.out.println("You chose a letter matrix.");
                     LettersMatrix lettersMatrix = new LettersMatrix();
                     lettersMatrix.calculateMatrix();
                     lettersMatrix.print();
                     break;
+
                 case 4: // transpose of a matrix
                     System.out.println("You chose to transpose a matrix.");
-
                     int rows4, columns4;
                     int [][] matrix1 = new int [10][10];
+                    System.out.println("Enter the number of rows and columns: ");
                     rows4 = in.nextInt();
                     columns4 = in.nextInt();
-
                     System.out.println("Enter your matrix!");
                     int i,j;
                     for(i = 0; i < rows4; i++)
@@ -56,9 +62,11 @@ public class App
                     transposedMatrix.calculateMatrix(matrix1);
                     transposedMatrix.print();
                     break;
+
                 case 5: // exit menu
                     ok = false;
                     break;
+
                 default:
                     System.out.println("Wrong input!!!");
                     break;
--- src/main/java/org/example/Matrix.java
+++ src/main/java/org/example/Matrix.java
@@ -11,11 +11,20 @@ public class Matrix {
     }
 
     int[][] calculateMatrix() { // method that calculates the matrix
-
         int i , j;
 
-
-
+        for(i = 1; i <= n; i++){
+            matrix[i][1] = i;
+        }
+        for(i = 1; i <= n; i++){
+            for(j = 2; j <= m; j++){
+                matrix[i][j] = matrix[i][j - 1] + 1;
+                if(matrix[i][j] > 9)
+                {
+                    matrix[i][j] -= 9;
+                }
+            }
+        }
 
         return matrix;
     }
@@ -23,7 +32,7 @@ public class Matrix {
     void print(){
         System.out.println("The matrix is: ");
         for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= n; j++)
+            for(int j = 1; j <= m; j++)
                 System.out.print(matrix[i][j]);
             System.out.println();
         }
